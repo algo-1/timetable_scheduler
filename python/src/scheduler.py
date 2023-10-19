ad16ec9403c62e927ac9e03e1879a6b8b4d5776c
Harold Karibiye <haroldkaribiye@gmail.com>
2023-10-19 13:32:33 +0100

    add initial files

---
+++ python/src/scheduler.py
@@ -0,0 +1,59 @@
+import xml.etree.ElementTree as ET
+
+# Define the path to your XHSTTS dataset XML file
+xml_file_path = "data/EnglandStPaul.xml"
+
+# Parse the XML file
+tree = ET.parse(xml_file_path)
+root = tree.getroot()
+
+# Iterate through 'Instance' elements
+for instance in root.findall(".//Instance"):
+    instance_id = instance.attrib["Id"]
+    instance_metadata = instance.find("MetaData")
+    instance_name = instance_metadata.find("Name").text
+
+    # Access other metadata or elements as needed
+    times = instance.find("Times")
+    resources = instance.find("Resources")
+    events = instance.find("Events")
+    constraints = instance.find("Constraints")
+
+    # Print or process data as necessary
+    print(f"Instance ID: {instance_id}")
+    print(f"Instance Name: {instance_name}")
+    print(times, resources, events, constraints)
+
+    # Example: To access specific data from 'Times'
+    for time in times.findall("Time"):
+        # Access time-related data as needed
+        pass
+
+    # Example: To access specific data from 'Resources'
+    for resource in resources.findall("Resource"):
+        # Access resource-related data as needed
+        pass
+
+    # Example: To access specific data from 'Events'
+    for event in events.findall("Event"):
+        # Access event-related data as needed
+        pass
+
+    # Example: To access specific data from 'Constraints'
+    for constraint in constraints.findall("Constraint"):
+        # Access constraint-related data as needed
+        pass
+
+    print("\n")
+
+# Iterate through 'SolutionGroup' elements
+for solution_group in root.findall(".//SolutionGroup"):
+    solution_group_metadata = solution_group.find("Metadata")
+    # Access metadata as needed
+
+    # Iterate through 'Solution' elements within 'SolutionGroup'
+    for solution in solution_group.findall("Solution"):
+        # Access solution-related data as needed
+
+        # Print or process data as necessary
+        pass
---
+++ python/src/utils.py
@@ -0,0 +1,40 @@
+from enum import Enum, auto
+from pathlib import Path
+
+
+class Cost_Function_Type(Enum):
+    Linear = auto()
+    Quadratic = auto()
+    Step = auto()
+
+
+def cost_function(deviation: int, cost_type: Cost_Function_Type):
+    """
+    deviation : is non-negative.
+    """
+    if cost_type == Cost_Function_Type.Linear:
+        return deviation
+    elif cost_type == Cost_Function_Type.Quadratic:
+        return deviation**2
+    elif cost_type == Cost_Function_Type.Step:
+        return 1 if deviation >= 0 else 0
+    else:
+        raise TypeError(
+            f"cost_type should be a Cost_Function_Type enum. Instead got {cost_type} of type {type(cost_type)}."
+        )
+
+
+def cost(deviation: int, constraint_weight: int, cost_type: Cost_Function_Type):
+    """
+    deviation : is non-negative.\n
+    constraint_weight : ranges from 0 to 100 inclusive.
+    """
+    return constraint_weight * cost_function(deviation, cost_type)
+
+
+def parse_xhtts(filename: Path | str):
+    pass
+
+
+if __name__ == "__main__":
+    pass
