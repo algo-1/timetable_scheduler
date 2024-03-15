4b41a18751d5d2328b4a9134217785d2f04bafb5
Harold Karibiye <haroldkaribiye@gmail.com>
2024-03-15 16:06:18 +0000

    update genetic

--- python/src/Algorithms/genetic2.py
+++ python/src/Algorithms/genetic2.py
@@ -32,6 +32,9 @@ class Solution:
     def is_feasible(self) -> bool:
         return self.cost.Infeasibility_Value == 0
 
+    def is_feasible_and_solves_objectives(self):
+        return self.cost.Infeasibility_Value == 0 and self.cost.Objective_Value == 0
+
 
 def mutate(solution: Solution, instance: XHSTTSInstance) -> None:
     # TODO: scrap deepcopy in this function
@@ -42,7 +45,6 @@ def mutate(solution: Solution, instance: XHSTTSInstance) -> None:
             # decide between mutating the time or one of the resources
             rand_num = random.randint(0, len(new_event.Resources))
             if rand_num == len(new_event.Resources):
-                # replace time ref
                 if not instance.Events[
                     event.InstanceEventReference
                 ].PreAssignedTimeReference:
@@ -150,7 +152,7 @@ def genetic_algorithm(
 ) -> list[XHSTTSInstance.SolutionEvent]:
     # Create a population of solutions.
 
-    global POPULATION_SIZE
+    global POPULATION_SIZE, NGEN
 
     population: list[Solution] = None
     best_random_solution = None
@@ -169,8 +171,13 @@ def genetic_algorithm(
             reverse=True,
         )
 
-        # set population size according to number of sol events (assumes each random solution has the same number of sol_events)
-        POPULATION_SIZE = min(POPULATION_SIZE, len(random_1000[0].sol_events) * 2)
+        # set population size & number of generations according to number of sol events (assumes each random solution has the same number of sol_events)
+        POPULATION_SIZE = (
+            len(random_1000[0].sol_events) * 2
+        )  # min(POPULATION_SIZE, len(random_1000[0].sol_events) * 2)
+        NGEN = max(NGEN, POPULATION_SIZE * 5)
+
+        print(f"population size = {POPULATION_SIZE}\nnumber of generations = {NGEN}")
 
         population = random_1000[:POPULATION_SIZE]
 
@@ -183,7 +190,11 @@ def genetic_algorithm(
     print("Generation Begin!")
     for idx in range(NGEN):
         # Select the best solutions from the population.
-        selected_solutions = tournament_selection(population, instance, 20)
+        num_selection = int(len(population) // 1.5)
+        # ensure even number
+        num_selection = num_selection + 1 if (num_selection % 2 != 0) else num_selection
+
+        selected_solutions = tournament_selection(population, instance, num_selection)
 
         # Crossover the selected solutions to produce new offspring.
         offspring: list[Solution] = []
@@ -216,7 +227,10 @@ def genetic_algorithm(
         # )
 
         best_solution = deepcopy(population[0])
-        if best_solution.is_feasible():
+        if best_solution.is_feasible_and_solves_objectives():
+            if not input_solution:
+                instance.evaluate_solution(best_random_solution.sol_events, debug=True)
+                print("\nbest random: ", best_random_solution.cost)
             return best_solution.sol_events
 
         print(f"Generation: {idx + 1}")
@@ -237,6 +251,9 @@ if __name__ == "__main__":
     dataset_sudoku4x4 = XHSTTS(data_dir.joinpath("ArtificialSudoku4x4.xml"))
     dataset_abramson15 = XHSTTS(data_dir.joinpath("ArtificialAbramson15.xml"))
     dataset_brazil3 = XHSTTS(data_dir.joinpath("BrazilInstance3.xml"))
+    aus_bghs98 = XHSTTS(data_dir.joinpath("AustraliaBGHS98.xml"))
+    aus_sahs96 = XHSTTS(data_dir.joinpath("AustraliaSAHS96.xml"))
+    aus_tes99 = XHSTTS(data_dir.joinpath("AustraliaTES99.xml"))
 
     dataset_names = {
         dataset_sudoku4x4: "ArtificialSudoku4x4",
@@ -246,7 +263,9 @@ if __name__ == "__main__":
 
     for dataset in (
         dataset_sudoku4x4,
-        dataset_abramson15,
+        # aus_bghs98,
+        aus_sahs96,
+        # aus_tes99,
     ):  # (dataset_sudoku4x4,):  # dataset_abramson15):
         random.seed(23)
 
@@ -261,13 +280,11 @@ if __name__ == "__main__":
         evaluation = instance.evaluate_solution(result, debug=True)
 
         print(
-            f"\n---Genetic Evaluation ({dataset_names[dataset]})---\n",
+            f"\n---Genetic Evaluation ({instance.name})---\n",
             evaluation,
         )
 
         # save the solution as an xml file
         solutions_dir = root_dir.joinpath("solutions")
-        file_path = solutions_dir.joinpath(
-            f"genetic_solution_{dataset_names[dataset]}.xml"
-        )
+        file_path = solutions_dir.joinpath(f"genetic_solution_{instance.name}.xml")
         XHSTTSInstance.sol_events_to_xml(result, instance, file_path)
